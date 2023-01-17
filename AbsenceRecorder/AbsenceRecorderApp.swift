23e5fba41d21c4c2f3b7d65654623d8c0fb39a2c
Lysander Myrddin-Evans <myrddin-evans/.l@etoncollege.org.uk>
2023-01-17 21:08:24 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Lysander Myrddin-Evans on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
