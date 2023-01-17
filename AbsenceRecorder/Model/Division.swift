da41b0e5945489301141752584a0014ac196c67a
Lysander Myrddin-Evans <myrddin-evans/.l@etoncollege.org.uk>
2023-01-17 21:22:02 +0000

    Adding basic model for divisions and students

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,17 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Lysander Myrddin-Evans on 17/01/2023.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
