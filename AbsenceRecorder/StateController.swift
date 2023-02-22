e2baa7ec1ce79a1398114e1908b4ac91cb518f92
Lysander Myrddin-Evans <myrddin-evans/.l@etoncollege.org.uk>
2023-02-22 17:38:30 +0000

    Absence Recorder Part 5

--- AbsenceRecorder/StateController.swift
+++ AbsenceRecorder/StateController.swift
@@ -8,9 +8,44 @@
 import Foundation
 
 class StateController: ObservableObject {
-    @Published var divisions: [Division]
+    @Published var divisions: [Division] = []
+    private var url: URL
     
     init() {
-        divisions = Division.examples
+        loadFromFile()
+    }
+    
+    func getPath(file: String) -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let userPath = paths[0]
+        let url = userPath.appendingPathComponent(file)
+        return url
+    }
+    
+    func loadFromFile() {
+        // get url to the file you want to load from
+        if let data = try? Data(contentsOf: getPath(file: "divisions.json") {
+            let decoder = JSONDecoder()
+            if let loaded = try? decoder.decode([Division].self, from: data) {
+                divisions = loaded
+            }
+        }
+    }
+    
+    func saveToFile() {
+        // create json encoder
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(divisions) {
+            // take encoded objects and convert them into a string
+            if let json = String(data: encoded, encoding: .utf8) {
+                // creat the file path
+                do {
+                    // write the json string to that file
+                    try json.write(to: getPath(file: "divisions.json"), atomically: true, encoding: .utf8)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
--- AbsenceRecorder/View/RootTabView.swift
+++ AbsenceRecorder/View/RootTabView.swift
@@ -29,5 +29,6 @@ struct RootTabView: View {
 struct RootTabView_Previews: PreviewProvider {
     static var previews: some View {
         RootTabView()
+            .environmentObject(StateController())
     }
 }
